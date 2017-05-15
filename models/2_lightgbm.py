5773c16bf0baf1c3dcc5bc1504169efa2117ca28
Takanori Hayashi <flowlight0@gmail.com>
2017-05-16 01:29:47 +0900

    Added takapt's feature

--- models/2_lightgbm.py
+++ models/2_lightgbm.py
@@ -4,6 +4,8 @@ import joblib
 import pandas as pd
 import numpy as np
 import sys
+
+from sklearn.metrics import log_loss
 from sklearn.model_selection import train_test_split
 
 import lightgbm as lgb
@@ -13,6 +15,13 @@ from models.utils import common_model_parser, log_result
 def add_feature_importance(bst, log_file: str):
     log_data = json.load(open(log_file))
     log_data['feature_importance'] = dict(zip(bst.feature_name(), bst.feature_importance().tolist()))
+    json.dump(log_data, open(log_file, 'w'), sort_keys=True, indent=4)
+
+
+def add_train_score(y_train, p_train, log_file: str, weight):
+    log_data = json.load(open(log_file))
+    log_data['results']['train_log_loss'] = log_loss(y_train, p_train, sample_weight=weight)
+    print(log_data)
     json.dump(log_data, open(log_file, 'w'), sort_keys=True, indent=4)
 
 
@@ -42,9 +51,13 @@ def main():
         params = config['model']['params']
         gbm = lgb.train(params['booster'], d_train, valid_sets=d_valid, **params['train'])
         joblib.dump(gbm, options.model_file)
+
+        p_train = gbm.predict(X_train, num_iteration=gbm.best_iteration)
         p_valid = gbm.predict(X_valid, num_iteration=gbm.best_iteration)
+
         log_result(y_valid, p_valid, config, options.log_file, weight=w_valid)
         add_feature_importance(gbm, options.log_file)
+        add_train_score(y_train=y_train, p_train=p_train, log_file=options.log_file, weight=w_train)
     else:
         gbm = joblib.load(options.model_file)
         data['is_duplicate'] = gbm.predict(data[feature_columns])
--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import argparse
 import glob
+import joblib
 import json
 import os
 import shlex
@@ -32,20 +33,6 @@ def get_feature_creator_file(feature_id: int):
     raise FileNotFoundError("File that matches feature_id ({}) doesn't exist".format(feature_id))
 
 
-def join_dataset(data_file: str, features_ids: list, tmp_file: str, is_train: bool):
-    feature_files = [feature_input_file(data_file, feature_id) for feature_id in features_ids]
-    with open(tmp_file, 'w') as f:
-        subprocess.call(shlex.split('paste -d, {0}'.format(" ".join(feature_files))), stdout=f)
-
-    if is_train:
-        data = pd.read_csv(tmp_file)
-        data['y'] = pd.read_csv(data_file)['is_duplicate'].values
-        data.to_csv(get_tmp_train_file(), index=False, float_format='%.5f')
-        return data
-    else:
-        return pd.read_csv(tmp_file)
-
-
 def scale_dataset(data, target_positive_ratio=0.165):
     if target_positive_ratio > 0:
         pos_data = data[data.y == 1]
@@ -75,23 +62,12 @@ def main():
     options = parser.parse_args()
     config = json.load(open(options.config_file))
 
-    for feature_id in config['features']:
-        feature_creator_file = get_feature_creator_file(feature_id)
-        if check_feature_existence(feature_creator_file, config['data_prefix']):
-            print('Feature file for {} exists.'.format(feature_creator_file), file=sys.stderr)
-            continue
-        commands = ["python3", feature_creator_file, "--data_prefix", config['data_prefix']]
-        if options.train_only:
-            commands.append("--train_only")
-        subprocess.call(commands)
-
-    data_files = dict(generate_filename_from_prefix(config['data_prefix']))
-
-    print("Started generating training dataset from features {}".format(config['features']))
+    data_files = generate_data_files(config, options)
+    takapt_features = config['takapt_features'] if 'takapt_features' in config else []
+    print("Started generating training dataset from features {} and {}".format(config['features'], takapt_features))
     start_time = time.time()
-    join_dataset(data_files['train'], config['features'], get_tmp_train_file(), True)
+    join_dataset(data_files['train'], config['features'], get_tmp_train_file(), True, config)
     print("Finished generating training dataset: {:.3f}s".format(time.time() - start_time))
-
     model_python = config['model']['path']
     prefix = os.path.basename(options.config_file) + '.{}'.format(os.path.basename(options.config_file))
     model_file = os.path.join('data/output', prefix + '.model')
@@ -102,15 +78,60 @@ def main():
                      '--log_file', output_file])
 
     if not options.train_only:
-        print("Started generating testing dataset from features {}".format(config['features']))
+        print("Started generating testing dataset from features {} and {}".format(config['features'], takapt_features))
         start_time = time.time()
-        join_dataset(data_files['test'], config['features'], get_tmp_test_file(), False)
+        join_dataset(data_files['test'], config['features'], get_tmp_test_file(), False, config)
         print("Finished generating testing dataset: {:.3f}s".format(time.time() - start_time))
 
         submission_file = os.path.join('data/output', prefix + '.submission.csv')
         subprocess.call(['python3', model_python, '--data_file', get_tmp_test_file(),
                          '--config_file', options.config_file, '--model_file', model_file,
                          '--submission_file', submission_file])
+
+
+def generate_data_files(config, options):
+    for feature_id in config['features']:
+        feature_creator_file = get_feature_creator_file(feature_id)
+        if check_feature_existence(feature_creator_file, config['data_prefix']):
+            print('Feature file for {} exists.'.format(feature_creator_file), file=sys.stderr)
+            continue
+        commands = ["python3", feature_creator_file, "--data_prefix", config['data_prefix']]
+        if options.train_only:
+            commands.append("--train_only")
+        subprocess.call(commands)
+    data_files = dict(generate_filename_from_prefix(config['data_prefix']))
+    return data_files
+
+
+def join_dataset(data_file: str, features_ids: list, tmp_file: str, is_train: bool, config: dict):
+    feature_files = [feature_input_file(data_file, feature_id) for feature_id in features_ids]
+    if 'takapt_features' in config:
+        df = pd.DataFrame()
+        takapt_file = 'tmp_takapt.csv'
+        for feature_name in config['takapt_features']:
+            data_prefix = config['data_prefix']
+            feature_file = os.path.join("data/takapt/feature/", os.path.basename(data_prefix), ("train_" if is_train else "test_") + feature_name + ".pkl")
+            feature = joblib.load(feature_file)
+            if is_train:
+                feature.drop(labels='id', axis=1, inplace=True)
+            else:
+                feature.drop(labels='test_id', axis=1, inplace=True)
+            for column in feature.columns:
+                df[column] = feature[column].values
+        df.to_csv(takapt_file, index=False)
+        feature_files.append(takapt_file)
+
+    with open(tmp_file, 'w') as f:
+        subprocess.call(shlex.split('paste -d, {0}'.format(" ".join(feature_files))), stdout=f)
+
+    if is_train:
+        data = pd.read_csv(tmp_file)
+        data['y'] = pd.read_csv(data_file)['is_duplicate'].values
+        data.to_csv(get_tmp_train_file(), index=False, float_format='%.5f')
+        print(data.columns)
+        return data
+    else:
+        return pd.read_csv(tmp_file)
 
 
 if __name__ == "__main__":
