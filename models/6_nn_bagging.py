8de954ca460cb79f7a563b9b7596084d176456b5
Takanori Hayashi <flowlight0@gmail.com>
2017-06-06 01:09:23 +0900

    Fix bug of 6_nn_bagging

--- models/6_nn_bagging.py
+++ models/6_nn_bagging.py
@@ -84,7 +84,7 @@ def main():
             X_valid = scaler.transform(X_valid.values)
 
             n_bags = 5
-            p_valid = np.zeros(X_valid.shape[0])
+            p_valid = np.zeros(X_train.shape[0])
             p_train = np.zeros(X_valid.shape[0])
             for j in range(n_bags):
                 model = nn_model(X_train)
